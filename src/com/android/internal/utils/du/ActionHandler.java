DirtyUnicorns/android_external_DUtils

DUI screenshot: add "partial" action and respect custom delay [1/3]
added a new icon for the action
Change-Id: I60f63474e9d660099c0bb4c883c4a22475b01039

src/com/android/internal/utils/du/ActionHandler.java
@@ -92,6 +92,7 @@ public class ActionHandler {
     public static final String SYSTEMUI_TASK_SETTINGS_PANEL = "task_settings_panel";
     public static final String SYSTEMUI_TASK_NOTIFICATION_PANEL = "task_notification_panel";
     public static final String SYSTEMUI_TASK_SCREENSHOT = "task_screenshot";
+    public static final String SYSTEMUI_TASK_REGION_SCREENSHOT = "task_region_screenshot";
     public static final String SYSTEMUI_TASK_SCREENRECORD = "task_screenrecord";
     // public static final String SYSTEMUI_TASK_AUDIORECORD =
     // "task_audiorecord";
@@ -134,6 +135,7 @@ public class ActionHandler {
     public static final String INTENT_SHOW_POWER_MENU = "action_handler_show_power_menu";
     public static final String INTENT_TOGGLE_SCREENRECORD = "action_handler_toggle_screenrecord";
     public static final String INTENT_SCREENSHOT = "action_handler_screenshot";
+    public static final String INTENT_REGION_SCREENSHOT = "action_handler_region_screenshot";
     public static final String INTENT_TOGGLE_FLASHLIGHT = "action_handler_toggle_flashlight";
 
     static enum SystemAction {
@@ -141,6 +143,7 @@ public class ActionHandler {
         SettingsPanel(SYSTEMUI_TASK_SETTINGS_PANEL, SYSTEMUI, "label_action_settings_panel", "ic_sysbar_settings_panel"),
         NotificationPanel(SYSTEMUI_TASK_NOTIFICATION_PANEL, SYSTEMUI, "label_action_notification_panel", "ic_sysbar_notification_panel"),
         Screenshot(SYSTEMUI_TASK_SCREENSHOT, SYSTEMUI, "label_action_screenshot", "ic_sysbar_screenshot"),
+        RegionScreenshot(SYSTEMUI_TASK_REGION_SCREENSHOT, SYSTEMUI, "label_action_region_screenshot", "ic_sysbar_region_screenshot"),
         Screenrecord(SYSTEMUI_TASK_SCREENRECORD, SYSTEMUI, "label_action_screenrecord", "ic_sysbar_record_screen"),
         ExpandedDesktop(SYSTEMUI_TASK_EXPANDED_DESKTOP, SYSTEMUI, "label_action_expanded_desktop", "ic_sysbar_expanded_desktop"),
         ScreenOff(SYSTEMUI_TASK_SCREENOFF, SYSTEMUI, "label_action_screen_off", "ic_sysbar_screen_off"),
@@ -206,7 +209,8 @@ public class ActionHandler {
             SystemAction.ImeArrowLeft, SystemAction.ImeArrowRight,
             SystemAction.ImeArrowUp, SystemAction.InAppSearch,
             SystemAction.VolumePanel, SystemAction.ClearNotifications,
-            SystemAction.EditingSmartbar, SystemAction.SplitScreen
+            SystemAction.EditingSmartbar, SystemAction.SplitScreen,
+            SystemAction.RegionScreenshot
     };
 
     public static class ActionIconResources {
@@ -450,6 +454,9 @@ public class ActionHandler {
             return;
         } else if (action.equals(SYSTEMUI_TASK_SCREENSHOT)) {
             takeScreenshot(context);
+            return;
+        } else if (action.equals(SYSTEMUI_TASK_REGION_SCREENSHOT)) {
+            takeRegionScreenshot(context);
             return;
         } else if (action.equals(SYSTEMUI_TASK_SCREENRECORD)) {
             takeScreenrecord(context);
@@ -844,6 +851,11 @@ public class ActionHandler {
 
     private static void takeScreenshot(Context context) {
         context.sendBroadcastAsUser(new Intent(INTENT_SCREENSHOT), new UserHandle(
+                UserHandle.USER_ALL));
+    }
+
+    private static void takeRegionScreenshot(Context context) {
+        context.sendBroadcastAsUser(new Intent(INTENT_REGION_SCREENSHOT), new UserHandle(
                 UserHandle.USER_ALL));
     }
 

mordiford/external_DUtils

src/com/android/internal/utils/du/ActionHandler.java
@@ -92,6 +92,7 @@ public class ActionHandler {
     public static final String SYSTEMUI_TASK_SETTINGS_PANEL = "task_settings_panel";
     public static final String SYSTEMUI_TASK_NOTIFICATION_PANEL = "task_notification_panel";
     public static final String SYSTEMUI_TASK_SCREENSHOT = "task_screenshot";
+    public static final String SYSTEMUI_TASK_REGION_SCREENSHOT = "task_region_screenshot";
     public static final String SYSTEMUI_TASK_SCREENRECORD = "task_screenrecord";
     // public static final String SYSTEMUI_TASK_AUDIORECORD =
     // "task_audiorecord";
@@ -136,6 +137,7 @@ public class ActionHandler {
     public static final String INTENT_SHOW_POWER_MENU = "action_handler_show_power_menu";
     public static final String INTENT_TOGGLE_SCREENRECORD = "action_handler_toggle_screenrecord";
     public static final String INTENT_SCREENSHOT = "action_handler_screenshot";
+    public static final String INTENT_REGION_SCREENSHOT = "action_handler_region_screenshot";
     public static final String INTENT_TOGGLE_FLASHLIGHT = "action_handler_toggle_flashlight";
 
     static enum SystemAction {
@@ -143,6 +145,7 @@ public class ActionHandler {
         SettingsPanel(SYSTEMUI_TASK_SETTINGS_PANEL, SYSTEMUI, "label_action_settings_panel", "ic_sysbar_settings_panel"),
         NotificationPanel(SYSTEMUI_TASK_NOTIFICATION_PANEL, SYSTEMUI, "label_action_notification_panel", "ic_sysbar_notification_panel"),
         Screenshot(SYSTEMUI_TASK_SCREENSHOT, SYSTEMUI, "label_action_screenshot", "ic_sysbar_screenshot"),
+        RegionScreenshot(SYSTEMUI_TASK_REGION_SCREENSHOT, SYSTEMUI, "label_action_region_screenshot", "ic_sysbar_region_screenshot"),
         Screenrecord(SYSTEMUI_TASK_SCREENRECORD, SYSTEMUI, "label_action_screenrecord", "ic_sysbar_record_screen"),
         ExpandedDesktop(SYSTEMUI_TASK_EXPANDED_DESKTOP, SYSTEMUI, "label_action_expanded_desktop", "ic_sysbar_expanded_desktop"),
         ScreenOff(SYSTEMUI_TASK_SCREENOFF, SYSTEMUI, "label_action_screen_off", "ic_sysbar_screen_off"),
@@ -213,8 +216,9 @@ public class ActionHandler {
             SystemAction.ImeArrowUp, SystemAction.InAppSearch,
             SystemAction.VolumePanel, SystemAction.ClearNotifications,
             SystemAction.EditingSmartbar, SystemAction.SplitScreen,
-            SystemAction.OneHandedModeLeft, SystemAction.OneHandedModeRight,
-            SystemAction.MediaArrowLeft, SystemAction.MediaArrowRight
+            SystemAction.RegionScreenshot, SystemAction.OneHandedModeLeft,
+            SystemAction.OneHandedModeRight, SystemAction.MediaArrowLeft,
+            SystemAction.MediaArrowRight
     };
 
     public static class ActionIconResources {
@@ -460,6 +464,9 @@ public class ActionHandler {
             return;
         } else if (action.equals(SYSTEMUI_TASK_SCREENSHOT)) {
             takeScreenshot(context);
+            return;
+        } else if (action.equals(SYSTEMUI_TASK_REGION_SCREENSHOT)) {
+            takeRegionScreenshot(context);
             return;
         } else if (action.equals(SYSTEMUI_TASK_SCREENRECORD)) {
             takeScreenrecord(context);
@@ -860,6 +867,11 @@ public class ActionHandler {
 
     private static void takeScreenshot(Context context) {
         context.sendBroadcastAsUser(new Intent(INTENT_SCREENSHOT), new UserHandle(
+                UserHandle.USER_ALL));
+    }
+
+    private static void takeRegionScreenshot(Context context) {
+        context.sendBroadcastAsUser(new Intent(INTENT_REGION_SCREENSHOT), new UserHandle(
                 UserHandle.USER_ALL));
     }
 